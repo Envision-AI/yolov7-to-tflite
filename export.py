bb78dd64185b1a6a936c80d33f3182a12151d811
Ubuntu <ubuntu@ip-172-31-43-116.eu-central-1.compute.internal>
2022-08-17 14:50:50 +0000

    export.py

---
+++ export.py
@@ -0,0 +1,66 @@
+import onnx
+import onnxruntime as ort
+import time
+import cv2
+import numpy as np
+import random
+from PIL import Image
+import tensorflow as tf
+import coremltools
+import matplotlib.pyplot as plt
+import argparse
+import sys
+import onnx
+from onnx_tf.backend import prepare
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--weights', type=str, default='./best.onnx', help='weights path')
+    parser.add_argument('--data', type=str, default='./coco.yaml', help='data required for quantization')
+    parser.add_argument('--img-size', nargs='+', type=int, default=[640, 640], help='image size')  # height, width
+    parser.add_argument('--device', default='cpu', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+    parser.add_argument('--include-nms', action='store_true', help='include nms on the tflite')
+    parser.add_argument('--int8', action='store_true', help='INT8 quantization')
+    opt = parser.parse_args()
+    opt.img_size *= 2 if len(opt.img_size) == 1 else 1  # expand
+    opt.dynamic = opt.dynamic and not opt.end2end
+    opt.dynamic = False if opt.dynamic_batch else opt.dynamic
+    print(opt)
+    t = time.time()
+    #load onnx model
+    try:
+        onnx_model = onnx.load("best.onnx")
+    except Exception as e:
+        print('ONNX load failure: %s' % e)
+    #convert to tf model
+    try:
+        tf_rep = prepare(onnx_model)
+        tf_rep.export_graph("best_tf.pb")
+    except Exception as e:
+        print('TF export failure: %s' % e)
+    #convert to tf lite
+    try:
+        converter = tf.lite.TFLiteConverter.from_saved_model("best_tf.pb")
+        converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS]
+        converter.target_spec.supported_types = [tf.float16]
+        converter.optimizations = [tf.lite.Optimize.DEFAULT]
+        # dataset = LoadImages(check_dataset(check_yaml(data))['train'], img_size=opt.img_size, auto=False)
+
+        if opt.int8:
+            converter.target_spec.supported_types = []
+            converter.representative_dataset = lambda: representative_dataset_gen(im[0])
+            data=opt.data
+            converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
+            converter.inference_input_type = tf.int8  # or tf.int8
+            converter.inference_output_type = tf.int8  # or tf.int8
+            converter.experimental_new_quantizer = True
+        if opt.include_nms:  
+            converter.target_spec.supported_ops.append(tf.lite.OpsSet.SELECT_TF_OPS)
+        tflite_model = converter.convert()
+    
+        open('best-tflite.tflite', "wb").write(tflite_model)
+    except Exception as e:
+        print('TFlite export failure: %s' % e)
+        
+    print('\nExport complete (%.2fs). Visualize with https://github.com/lutzroeder/netron.' % (time.time() - t))
