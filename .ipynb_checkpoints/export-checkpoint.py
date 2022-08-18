b37b208552d99c66752b64ba8909e212e992489d
Ubuntu <ubuntu@ip-172-31-43-116.eu-central-1.compute.internal>
2022-08-18 14:33:54 +0000

    export py fixes

--- .ipynb_checkpoints/export-checkpoint.py
+++ .ipynb_checkpoints/export-checkpoint.py
@@ -13,11 +13,18 @@ import sys
 import onnx
 from onnx_tf.backend import prepare
 
-
+def representative_dataset_gen(img):
+    # Representative dataset generator for use with converter.representative_dataset, returns a generator of np arrays
+    # im = np.transpose(img, [1, 2, 0])
+    im=img
+    im = np.expand_dims(im, axis=0).astype(np.float32)
+    im /= 255
+    yield [im]
+        
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--weights', type=str, default='./best.onnx', help='weights path')
-    parser.add_argument('--data', type=str, default='./coco.yaml', help='data required for quantization')
+    parser.add_argument('--data', type=str, default='./example.png', help='data required for quantization')
     parser.add_argument('--img-size', nargs='+', type=int, default=[640, 640], help='image size')  # height, width
     parser.add_argument('--device', default='cpu', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
     parser.add_argument('--include-nms', action='store_true', help='include nms on the tflite')
@@ -49,8 +56,13 @@ if __name__ == '__main__':
 
         if opt.int8:
             converter.target_spec.supported_types = []
-            converter.representative_dataset = lambda: representative_dataset_gen(im[0])
-            data=opt.data
+            image=cv2.imread(opt.data)
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            image = image.transpose((2, 0, 1))
+            image = np.ascontiguousarray(image)
+            im = image.astype(np.float32)
+            im =im/255
+            converter.representative_dataset = lambda: representative_dataset_gen(im)
             converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
             converter.inference_input_type = tf.int8  # or tf.int8
             converter.inference_output_type = tf.int8  # or tf.int8
